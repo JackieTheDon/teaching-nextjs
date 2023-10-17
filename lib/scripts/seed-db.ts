ce764f9fd68ba86eef5d97d08f37ddcd0386fa4d
JackieTheDon <91138276+JackieTheDon@users.noreply.github.com>
2023-10-17 08:08:38 +0000

    bruh

--- lib/scripts/seed-db.ts
+++ lib/scripts/seed-db.ts
@@ -1,4 +1,6 @@
 import { createDB } from '../../src/lib/db'
+import fs from 'fs/promises'
+import { faker } from '@faker-js/faker'
 
 async function seedDB() {
   console.log('Seeding database...')
@@ -6,22 +8,30 @@ async function seedDB() {
   const db = createDB()
 
   await db.deleteFrom('products').execute()
+  await db.deleteFrom('products_reviews').execute()
 
-  await db
-    .insertInto('products')
-    .values([
-      {
-        name: 'iPhone 12',
-        description: 'Apple IPHONE',
-        price: 700,
-      },
-      {
-        name: 'iPhone 11',
-        description: 'Apple old IPHONE',
-        price: 600,
-      },
-    ])
-    .execute()
+  const data = []
+
+  for (let i = 0; i < 10; i++) {
+    data.push({
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      price: faker.number.int(2000),
+    })
+  }
+  const productRev = await db.insertInto('products').values(data).returning('id').execute()
+
+  for (const dataRev of productRev) {
+    db.insertInto('products_reviews')
+      .values([
+        {
+          product_id: dataRev.id,
+          rating: faker.number.int(10),
+          content: faker.lorem.paragraph(100),
+        },
+      ])
+      .execute()
+  }
 
   console.log('Done')
 }
